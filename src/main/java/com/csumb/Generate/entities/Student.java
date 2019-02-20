c63007036aa3431a986880ef397e38c2650fe543
unknown <dochoa-aguila@csumb.edu>
2019-02-20 12:44:45 -0800

    init entities

---
+++ src/main/java/com/csumb/Generate/entities/Student.java
@@ -0,0 +1,54 @@
+package com.csumb.Generate.entities;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document
+public class Student {
+
+    @Id
+    String per_id;
+    String name;
+    int grade;
+    List prefered_classes;
+
+    public Student(String per_id, String name, int grade) {
+        this.per_id = per_id;
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public String getPer_id() {
+        return per_id;
+    }
+
+    public void setPer_id(String per_id) {
+        this.per_id = per_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public List getprefered_classes() {
+        return prefered_classes;
+    }
+
+    public void setprefered_classes(List prefered_classes) {
+        this.prefered_classes = prefered_classes;
+    }
+}
---
+++ src/main/java/com/csumb/Generate/entities/Teacher.java
@@ -0,0 +1,53 @@
+package com.csumb.Generate.entities;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Teacher {
+
+    @Id
+    String id;
+    String name;
+    String department;
+    int prep;
+
+    public Teacher(String id, String name, String department, int prep) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+        this.prep = prep;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getPrep() {
+        return prep;
+    }
+
+    public void setPrep(int prep) {
+        this.prep = prep;
+    }
+}
