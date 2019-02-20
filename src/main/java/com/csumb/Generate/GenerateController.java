aa6d16d35d06a281daf4836b7af1a3ca2bd19faa
rami9284 <judramirez@csumb.edu>
2019-02-20 13:56:09 -0800

    Fixed errors1

--- src/main/java/com/csumb/Generate/GenerateController.java
+++ src/main/java/com/csumb/Generate/GenerateController.java
@@ -3,6 +3,8 @@ package com.csumb.Generate;
 import java.util.List;
 
 import com.csumb.Generate.entities.Student;
+import com.csumb.Generate.repositotries.IStudentRepository;
+import com.csumb.Generate.repositotries.ITeacherRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GenerateController{
     @Autowired
-    Student student;
+    IStudentRepository student;
+    ITeacherRepository teacher; 
 
     @GetMapping("/students")
 
