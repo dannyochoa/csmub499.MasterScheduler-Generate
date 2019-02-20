f56fe6cbfea08139d8d354f3a7aed45056685a41
unknown <dochoa-aguila@csumb.edu>
2019-02-20 13:10:05 -0800

    adding init repos and properties

---
+++ src/main/java/com/csumb/Generate/repositotries/IStudentRepository.java
@@ -0,0 +1,11 @@
+package com.csumb.Generate.repositotries;
+
+import com.csumb.Generate.entities.Student;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IStudentRepository extends MongoRepository<Student,String> {
+
+}
+
