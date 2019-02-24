0f8070dc32d6b1ab07371aa7992f6268374f6879
unknown <dochoa-aguila@csumb.edu>
2019-02-24 00:37:47 -0800

    checking status

--- src/main/java/com/csumb/Generate/repositotries/IStudentRepository.java
+++ src/main/java/com/csumb/Generate/repositotries/IStudentRepository.java
@@ -3,7 +3,7 @@ package com.csumb.Generate.repositotries;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import com.csumb.Generate.entities.Student;
-
+   
 @Repository
 public interface IStudentRepository extends MongoRepository<Student,String> {
 
