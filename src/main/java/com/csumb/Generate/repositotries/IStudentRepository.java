614ae4163586c69032f8c2540a477dacb9ba02ab
rami9284 <judramirez@csumb.edu>
2019-03-01 09:38:43 -0800

    minor changes

--- src/main/java/com/csumb/Generate/repositotries/IStudentRepository.java
+++ src/main/java/com/csumb/Generate/repositotries/IStudentRepository.java
@@ -1,13 +1,11 @@
 package com.csumb.Generate.repositotries;
 
-import com.csumb.Administrative.entities.Student;
+import com.csumb.Generate.entities.Student;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IStudentRepository extends MongoRepository<Student,String> {
 
-   // @Query(value = "{'per_id': '$0'}", delete = true)
-   // void deleteById(String id);
 }
 
--- src/main/java/com/csumb/Generate/repositotries/ITeacherRepository.java
+++ src/main/java/com/csumb/Generate/repositotries/ITeacherRepository.java
@@ -1,10 +1,10 @@
 package com.csumb.Generate.repositotries;
 
-import com.csumb.Administrative.entities.Teacher;
+import com.csumb.Generate.entities.Teacher;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ITeacherRepository extends MongoRepository<Teacher,String> {
+public interface ITeacherRepository extends MongoRepository<Teacher, String> {
 
 }
